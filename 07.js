169ef3d00ea868f9170b46e4bbb811b8d8aa9728
Ogura <non01211983@yahoo.co.jp>
2023-12-11 02:11:38 +0000

    07_1211

---
+++ 07.js
@@ -0,0 +1,9 @@
+let fruit = {apple:'りんご',strawberry:'いちご',grape:'ぶどう',lemon:'レモン'};
+
+console.log(fruit.grape);
+
+console.log(Object.keys(fruit));
+
+console.log(Object.values(fruit));
+
+// console.log(fruit);
