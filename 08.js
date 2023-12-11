cc360f94b54f427dcbab5e0ef519f9ab6cd7a4da
Ogura <non01211983@yahoo.co.jp>
2023-12-11 02:59:05 +0000

    08_1211

---
+++ 08.js
@@ -0,0 +1,18 @@
+function triangle(bottom,height){
+    
+    let area = bottom*height/2;
+    
+    return area;
+}
+
+console.log(triangle(10,20));
+
+
+let triangle_2 = function(bottom,height){
+    
+    let area = bottom*height/2;
+    
+    return area;
+}
+
+console.log(triangle_2(20,40));
