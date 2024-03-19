25a1551f5aed8c019864a2bc6b6d5df39185e54d
Ogura <non01211983@yahoo.co.jp>
2024-03-19 03:57:37 +0000

    0319_16

---
+++ 16_08.js
@@ -0,0 +1,13 @@
+// jQueryの場合
+$(function () {
+  $('#id_01').removeClass('class_01');
+  $('#id_02').removeClass('class_01').addClass('class_02');
+  $('#id_03').addClass('hidden');
+});
+
+//document.getElementById('id_01').classList.remove('class_01');
+
+//document.getElementById('id_02').classList.remove('class_01')
+//document.getElementById('id_02').classList.add('class_02');
+
+//document.getElementById('id_03').classList.add('hidden');
---
+++ 16_12.js
@@ -0,0 +1,6 @@
+$(function () {
+  $('#btn').on('click', function() {
+    // ここに処理を記述してください
+    $('#msg').text('ボタンが押されました').css('color', 'blue');
+  });
+});
